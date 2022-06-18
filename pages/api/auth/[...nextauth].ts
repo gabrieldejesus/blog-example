ce03d97803e0b10201ac3c83798759a6de5bab3b
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2022-06-18 11:38:34 -0300

    feat: add log in functionality

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import { NextApiHandler } from 'next'
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GitHubProvider from 'next-auth/providers/github'
+
+// utils
+import prisma from '../../../lib/prisma'
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+}
+
+const autoHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
+export default autoHandler
